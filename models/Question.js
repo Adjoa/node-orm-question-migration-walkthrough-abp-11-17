e64f2dfe7bacdf0dbd7357be39e5486aa2326e6f
Adjoa Darien <adjoa.darien@gmail.com>
2017-11-17 13:39:49 +0000

    automatically backed up by learn

--- models/Question.js
+++ models/Question.js
@@ -15,10 +15,10 @@ class Question {
     //console.log("Preparing to create questions table...")
 
     return new Promise(function(resolve){
-      //db.run(sql, function(){
+      db.run(sql, function(){
         //console.log("...questions table created!")
-        resolve("Success!")
-      //})
+        resolve("Created questions table!")
+      })
     })
   }
 }
